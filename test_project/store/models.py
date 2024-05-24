ebf5ce0268118ea5ff119087fac849ac16c2ac0d
Slava <steschenko.vyacheslav@gmail.com>
2024-05-24 14:25:53 +0200

    Working on permissions

--- test_project/store/models.py
+++ test_project/store/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'ID{self.id}. {self.name} - {self.price} - {self.author_name}'
--- test_project/store/views.py
+++ test_project/store/views.py
@@ -3,17 +3,21 @@ from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrStaffOrReadOnly
 from django.shortcuts import render
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_field = ['price', 'author_name', 'id']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 def auth(request):
     return render(request, 'oauth.html')
