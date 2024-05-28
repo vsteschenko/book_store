b489766b77cdf1195cfb86e3f1cc915263c81209
Slava <steschenko.vyacheslav@gmail.com>
2024-05-28 11:51:15 +0200

    Added new model to like, rate and save in bookmarks, covered with tests

--- test_project/store/views.py
+++ test_project/store/views.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, UserBookRelation
+from .serializers import BookSerializer, UserBookRelationSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .permissions import IsOwnerOrStaffOrReadOnly
 from django.shortcuts import render
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
@@ -18,6 +21,18 @@ class BookViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.validated_data['owner'] = self.request.user
         serializer.save()
+
+class UserBookRelationView(UpdateModelMixin, GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = UserBookRelation.objects.all()
+    serializer_class = UserBookRelationSerializer
+    #для удобства на фронте
+    lookup_field = 'book'
+
+    def get_object(self):
+        obj, created = UserBookRelation.objects.get_or_create(user=self.request.user, book_id=self.kwargs['book'])
+        print('created', created)
+        return obj
 
 def auth(request):
     return render(request, 'oauth.html')
